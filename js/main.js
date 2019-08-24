bd1bc52a768e38572258feb56cb78d8f35f639ab
Nana <nastyaa1212@gmail.com>
2019-08-24 17:18:55 +0300

    deep freezing init

---
+++ js/main.js
@@ -0,0 +1,45 @@
+function deepFreeze (object) {
+	
+	if(typeof object !== 'object'){
+		throw new Error(`argument must be type of object, ${typeof object} given.`);
+	}
+
+	for(let prop in object){
+		
+		if( typeof object[prop] === 'object' ){
+
+			deepFreeze(object[prop]);
+		}
+
+		let descriptor = Object.getOwnPropertyDescriptor(object, prop);
+		
+		if(!descriptor){
+			console.log(prop);
+		}
+
+		descriptor.writable = false;
+		descriptor.enumerable = false;
+
+		Object.defineProperty(object, prop, descriptor);
+
+	}
+}
+
+const nana = {
+	name : "nana",
+	age : 19,
+	friends : [
+		{
+			name : "tanya",
+			age : 45, 
+			hobby : ["baking", "racing"]
+		},
+		{
+			name : "alex",
+			age : 22, 
+			parents : [ "mama", "papa" ]
+		}
+
+	]};
+
+deepFreeze(nana);
